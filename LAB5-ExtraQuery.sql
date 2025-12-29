679c4868394121f64776ee590d5b209ff8294b7a
Manav Delvadiya <24010101056@darshan.ac.in>
2025-12-29 14:43:09 +0530

    Add files via upload

---
+++ LAB5-ExtraQuery.sql
@@ -0,0 +1,40 @@
+﻿-- Part- D (Basic UPDATE Practice)
+
+-- Update the deposit amount of customer SUNIL to 6000.
+
+-- Change branch name of account number 110 to POWAI.
+
+-- Update deposit amount to NULL where customer name is KRANTI.
+
+-- Modify account number 108 to 208.
+
+-- Change branch name ANDHERI to ANDHERI WEST in DEPOSIT table.
+
+-------------------------------------------------------------------------------
+
+-- Part- E (Conditional UPDATE)
+
+-- Increase deposit amount by ₹500 for customers whose deposit amount is less than 3000.
+
+-- Update deposit amount to 8000 where branch name is POWAI.
+
+-- Change branch name to AJNI where account number is greater than 105.
+
+-- Update deposit date to 01-JAN-96 where branch name is VRCE.
+
+-- Update deposit amount to 4000 for customers whose name starts with S.
+
+-------------------------------------------------------------------------------
+
+
+-- Part- F (Multiple Column UPDATE)
+
+-- Update branch name to DHARAMPETH and amount to 6500 where account number is 109.
+
+-- Change customer name MEHUL to MEHUL PATEL and amount to 4500.
+
+-- Modify branch name to NEHRU PLACE and deposit date to 15-AUG-95 where amount is 5000.
+
+-- Update amount = 9000 and branch name = VIRAR where customer name is MINU.
+
+-- Change account number to 999 and amount to 3000 where branch name is BANDRA.
