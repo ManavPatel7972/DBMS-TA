3fa589d9acfa24b0868921160f08094a6590bb8d
Manav Delvadiya <24010101056@darshan.ac.in>
2026-01-07 14:37:06 +0530

    Lab-08 Extra Practice

---
+++ Lab-08_Extra.sql
@@ -0,0 +1,45 @@
+CREATE TABLE Employee (
+    EmpId INT,
+    EmpName VARCHAR(30),
+    Salary DECIMAL(10,2),
+    JoinDate DATETIME
+);
+
+-- Part A – ALTER TABLE (ADD Columns):
+
+-- Add a column Department of type VARCHAR(20) to the Employee table.
+
+-- Add a column Email of type VARCHAR(40).
+
+-- Add two columns City VARCHAR(20) and State VARCHAR(20) in a single query.
+
+-- Add a column Experience of type INT.
+
+-- Add a column Gender of type VARCHAR(10).
+
+
+-- Part B – ALTER TABLE (MODIFY / CHANGE Data Type):
+
+-- Change the datatype of EmpName from VARCHAR(30) to VARCHAR(50).
+
+-- Change the datatype of Salary from DECIMAL(10,2) to DECIMAL(12,2).
+
+-- Change the datatype of JoinDate from DATETIME to DATE.
+
+-- Change the datatype of Department from VARCHAR(20) to VARCHAR(30).
+
+-- Change the datatype of Email from VARCHAR(40) to VARCHAR(60).
+
+
+
+-- Part C – ALTER TABLE (RENAME Column):
+
+-- Rename column EmpId to EmployeeId.
+
+-- Rename column EmpName to EmployeeName.
+
+-- Rename column JoinDate to JoiningDate.
+
+-- Rename column Salary to MonthlySalary.
+
+-- Rename column Experience to TotalExperience.
