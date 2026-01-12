40da626e4d74cf52c8c546e2f2bdbd6362fa3315
Manav Delvadiya <24010101056@darshan.ac.in>
2026-01-12 14:28:22 +0530

    Extra Question Math Function

---
+++ Lab-10_Extra.sql
@@ -0,0 +1,38 @@
+-- EASY
+
+-- Display the sum of 10 and 5.
+
+-- Display the subtraction of 8 from 20.
+
+-- Display the multiplication of 6 and 7.
+
+-- Display the division of 40 by 5.
+
+-- Find the absolute value of −15.
+
+-- Find the square of 9.
+
+-- Find the square root of 16.
+
+-- Find the value of 2 raised to the power 3.
+
+-- Find the remainder when 9 is divided by 2.
+
+-- Round the number 45.678 to 2 decimal places.
+
+-- Find the ceiling value of 12.3.
+
+-- Find the floor value of 12.9.
+
+-- Display the value of PI.
+
+-- Generate a random number.
+
+-- Find the sign of −25.
+
+--HARD
+-- Find the difference between the CEILING and FLOOR of 27.8.
+
+-- Generate a random integer between 1 and 50.
+
+-- Find the absolute difference between –250 and 175.
