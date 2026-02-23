483caa44bb7c86fda96299a97ff02637e97959bc
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-02-23 08:09:30 +0530

    Add Lab-20 Schema file

---
+++ Lab-20.sql
@@ -0,0 +1,22 @@
+
+CREATE TABLE Computer(
+    RollNo INT,
+    Name VARCHAR(20)
+);
+
+CREATE TABLE Electrical(
+    RollNo INT,
+    Name VARCHAR(20)
+);
+
+INSERT INTO Computer VALUES
+(101,'Ajay'),
+(105,'Haresh'),
+(107,'Mahesh'),
+(115,'Manish');
+
+INSERT INTO Electrical VALUES
+(109,'Ajay'),
+(115,'Manish');
+
+
