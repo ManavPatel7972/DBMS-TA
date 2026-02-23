29d7eeb95614748bf177748a65b3735da7cf5d84
Manav Delvadiya <manavdelvadiya7972@gmail.com>
2026-02-23 08:13:41 +0530

    Add Part-B Schema

--- Lab-20.sql
+++ Lab-20.sql
@@ -1,4 +1,6 @@
 
+-- --------------------------------------PART-A--------------------------------------
+
 CREATE TABLE Computer(
     RollNo INT,
     Name VARCHAR(20)
@@ -19,4 +21,28 @@ INSERT INTO Electrical VALUES
 (109,'Ajay'),
 (115,'Manish');
 
+------------------------------------PART-B--------------------------------------
 
+CREATE TABLE Employee_A(
+    EmpID INT,
+    EName VARCHAR(20),
+    Department VARCHAR(20)
+);
+
+CREATE TABLE Employee_B(
+    EmpID INT,
+    EName VARCHAR(20),
+    Department VARCHAR(20)
+);
+
+INSERT INTO Employee_A VALUES
+(101,'Amar','Sales'),
+(102,'Boby','IT'),
+(103,'Charlie','HR'),
+(104,'Denish','Sales');
+
+INSERT INTO Employee_B VALUES
+(101,'Amar','Sales'),
+(103,'Charlie','HR'),
+(105,'Isha','Finance'),
+(106,'Fatima','IT');
